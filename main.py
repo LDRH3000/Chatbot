96b97782abb00580677d869bf78bc84071460443
LDRH3000 <hasan.istmhn@gmail.com>
2024-05-04 22:17:27 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -7,23 +7,35 @@ orders = orderData.data
 
 def control(request):
     # bu kısımda chatbot un yapay zeka ile istenilen bilgiyi doğru analiz etmesini sağlayacağız
+    response = ""
 
     if ('nerde' in request) or ('konum' in request) or ('detay' in request) or ('sipar' in request) or ('bilgi' in request) or ('nerede' in request)  :
-        x =0
-        response = ""
+        
         for order in orders:
             if   str(order["name"]).lower() in request:
-                x += 1
 
                 if str(order["cargoStatu"]) == "Teslim Edildi":
                     response += f'{order["name"]} siparişiniz {order["price"]} tutarında {order["deliveryDate"]} tarihinde {order["cargoName"]} tarafından teslim edilmiştir \n'
                 else:
                     response +=(f'{order["name"]} siparişiniz durumu {order["cargoStatu"]} olaran görünmektedir. {order["cargoName"]} tarafından {order["deliveryDate"]} tarihinde sizlere iletilecektir \n')
         
-        return response
-    else:
-        return "Ne söylediğini anlayamadım. Tekrar Eder misin"
+
     
+
+    elif ('ulaşmadı' in request) or ('gelmedi' in request) or ('varmadı' in request):
+        for order in orders:
+            if  str(order["name"]).lower() in request:
+                if  str(order["cargoStatu"]) == "Teslim Edildi":
+                    response += ("Teknik bir hata oluştu ürününüzü tekrar yolluyoruz. Anlayışınız için teşekkür ederiz")
+                else:
+                    response += (f'{order["name"]} siparişinşz şu anda {order["cargoStatu"]} durumundadır. ürününüz {order["deliveryDate"]} tarihinde teslim edilecektir')
+    else:
+        response = 'Ne söylediğini anlayamadım. Tekrar Eder misin'     
+
+        
+    
+        
+    return response
 
 while True:
 
